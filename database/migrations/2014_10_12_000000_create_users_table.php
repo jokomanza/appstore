6ca8d1cf9037c8dcdb98faffdb563d78253aa11f
Joko Supriyanto <jokomanza@gmail.com>
2022-02-26 19:10:03 +0700

    add user level

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,15 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
+            $table->string('registration_number');
             $table->string('name');
             $table->string('email')->unique();
+            $table->enum('access_level', [1, 2, 3]);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+
+            $table->primary('registration_number');
         });
     }
 
