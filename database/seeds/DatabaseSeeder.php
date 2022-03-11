ebab7a196e8edcb7e33a593b762a2cda15176bdf
Joko Supriyanto <jokomanza@gmail.com>
2022-03-11 19:32:12 +0700

    update seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,5 +15,21 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
 
         factory(App\Models\App::class, 100)->create();
+
+        DB::table('users')->insert([
+            'name' => 'Joko Supriyanto',
+            'email' => 'joko_supriyanto@quick.com',
+            'access_level' => 3,
+            'password' => bcrypt('123456'),
+        ]);
+
+        DB::table('apps')->insert([
+            'name' => 'Quick App Store',
+            'package_name' => 'com.quick.quickappstore',
+            'description' => 'Aplikasi untuk mengelola semua aplikasi',
+            'type' => 'Tool App',
+            'icon_url' => 'https://google.com',
+            'repository_url' => 'http:git.quick.com/production/quick-appstore-mobile.git',
+        ]);
     }
 }
