cd15ceb75b1a328247b9a293195ebbfd062ddfde
Joko Supriyanto <joko_supriyanto@quick.com>
2022-06-10 13:09:44 +0700

    remove route package name app validation

--- routes/web/user/routes.php
+++ routes/web/user/routes.php
@@ -56,30 +56,24 @@ Route::get('/apps', 'UserAppController@index')
     ->name('app.index');
 
 Route::get('/app/{packageName}', 'UserAppController@show')
-    ->where('packageName', 'com.quick.[a-z0-9_]{3,30}$')
     ->name('app.show');
 
 Route::get('/app/{packageName}/edit', 'UserAppController@edit')
-    ->where('packageName', 'com.quick.[a-z0-9_]{3,30}$')
     ->name('app.edit');
 
 Route::put('/app/{packageName}', 'UserAppController@update')
-    ->where('packageName', 'com.quick.[a-z0-9_]{3,30}$')
     ->name('app.update');
 
 Route::delete('/app/{packageName}', 'UserAppController@destroy')
-    ->where('packageName', 'com.quick.[a-z0-9_]{3,30}$')
     ->name('app.destroy');
 
 
 // apps permission
 /*----------------------------------------------------------------------------------------------------------------*/
 Route::post('/app/{packageName}/permission', 'UserAppController@addPermission')
-    ->where('packageName', 'com.quick.[a-z0-9_]{3,30}$')
     ->name('app.permission.store');
 
 Route::delete('/app/{packageName}/permission/{registrationNumber}', 'UserAppController@removePermission')
-    ->where('packageName', 'com.quick.[a-z0-9_]{3,30}$')
     ->where('registrationNumber', '[A-Z]{1}[0-9]{4}$')
     ->name('app.permission.destroy');
 
@@ -89,34 +83,27 @@ Route::delete('/app/{packageName}/permission/{registrationNumber}', 'UserAppCont
 Route::post('apps/datatables', 'UserAppController@getDataTables')
     ->name('app.datatables');
 Route::post('app/{packageName}/reports/datatables', 'UserReportController@getDataTables')
-    ->where('packageName', 'com.quick.[a-z0-9_]{3,30}$')
     ->name('app.report.datatables');
 
 
 // app versions
 /*----------------------------------------------------------------------------------------------------------------*/
 Route::get('/app/{packageName}/version', 'UserAppVersionController@create')
-    ->where('packageName', 'com.quick.[a-z0-9_]{3,30}$')
     ->name('version.create');
 
 Route::post('/app/{packageName}/version', 'UserAppVersionController@store')
-    ->where('packageName', 'com.quick.[a-z0-9_]{3,30}$')
     ->name('version.store');
 
 Route::get('/app/{packageName}/version/{versionName}', 'UserAppVersionController@show')
-    ->where('packageName', 'com.quick.[a-z0-9_]{3,30}$')
     ->name('version.show');
 
 Route::get('/app/{packageName}/version/{versionName}/edit', 'UserAppVersionController@edit')
-    ->where('packageName', 'com.quick.[a-z0-9_]{3,30}$')
     ->name('version.edit');
 
 Route::put('/app/{packageName}/version/{versionName}', 'UserAppVersionController@update')
-    ->where('packageName', 'com.quick.[a-z0-9_]{3,30}$')
     ->name('version.update');
 
 Route::delete('/app/{packageName}/version/{versionName}', 'UserAppVersionController@destroy')
-    ->where('packageName', 'com.quick.[a-z0-9_]{3,30}$')
     ->name('version.destroy');
 
 
@@ -127,12 +114,10 @@ Route::get('/notifications/report/{id}', 'UserReportController@showReportFromNot
     ->name('notification.report.show');
 
 Route::get('/app/{packageName}/report/{id}', 'UserReportController@show')
-    ->where('packageName', 'com.quick.[a-z0-9_]{3,30}$')
     ->where('id', '^[0-9]*$')
     ->name('report.show');
 
 Route::delete('/app/{packageName}/report/{id}', 'UserReportController@destroy')
-    ->where('packageName', 'com.quick.[a-z0-9_]{3,30}$')
     ->where('id', '^[0-9]*$')
     ->name('report.destroy');
 
